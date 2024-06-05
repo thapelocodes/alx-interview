1a901ca7ab1233a224e2e51878920e67f5839f30
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-05 12:47:07 +0200

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require("request");
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charArray = JSON.parse(res.body).characters;
+  for (const char of charArray) {
+    await new Promise((resolve, reject) => {
+      request(char, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
