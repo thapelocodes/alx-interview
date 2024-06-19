6c9390a912855d9fcd0af13865381762697a8843
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-20 01:06:55 +0200

    fixing accuracy

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-''' Making Change '''
+""" Making changes """
 
 
 def makeChange(coins, total):
-    ''' Finds the least number of coins that add up to a given sum. '''
+    """ Finds the least number of coins that add up to a given sum. """
     if total <= 0:
         return 0
     
     coins.sort(reverse=True)
-    counter = 0
-    
+    temp = 0
     for coin in coins:
-        while(total >= coin):
+        while total >= coin:
             total -= coin
-            counter += 1
-
-    if total == 0:
-        return counter
-    total += coin
-    counter -= 1
+            temp += 1
+        if total == 0:
+            return temp
     
     return -1
