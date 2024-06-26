e5aafcc8769944bc5551b55eb137d1e88acab0aa
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-26 14:37:30 +0200

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Calculates the perimeter of a single island in a grid. """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += j - 1 < 0 or grid[i][j -1] == 0                # Left
+                perimeter += i - 1 < 0 or grid[i - 1][j] == 0               # Top
+                perimeter += j + 1 == len(grid[i]) or grid[i][j + 1] == 0   # Right
+                perimeter += i + 1 == len(grid) or grid[i + 1][j] == 0      # Bottom
+    
+    return perimeter
