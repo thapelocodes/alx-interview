685a959057679a3c1b8516d00350efd7c29fe8b5
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-26 09:01:06 +0200

    Fix PEP8 conditions

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,13 +8,14 @@ def makeChange(coins, total):
     Args:
         coins (list): List of coin denominations available.
         total (int): Total amount needed.
-        
+
     Returns:
-        int: Minimum number of coins needed to make the total, or -1 if not possible.
+        int: Minimum number of coins needed to make the total,
+             or -1 if not possible.
     """
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     counter = 0
     for coin in coins:
@@ -23,5 +24,5 @@ def makeChange(coins, total):
             counter += 1
         if total == 0:
             return counter
-    
+
     return -1
