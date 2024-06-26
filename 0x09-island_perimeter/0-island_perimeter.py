14d09e790a0d58ae4c35a0337854b3e49d6c3da7
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-06-26 14:47:10 +0200

    PEP8 fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,9 +9,9 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += j - 1 < 0 or grid[i][j -1] == 0                # Left
-                perimeter += i - 1 < 0 or grid[i - 1][j] == 0               # Top
-                perimeter += j + 1 == len(grid[i]) or grid[i][j + 1] == 0   # Right
-                perimeter += i + 1 == len(grid) or grid[i + 1][j] == 0      # Bottom
-    
+                perimeter += j - 1 < 0 or grid[i][j - 1] == 0
+                perimeter += i - 1 < 0 or grid[i - 1][j] == 0
+                perimeter += j + 1 == len(grid[i]) or grid[i][j + 1] == 0
+                perimeter += i + 1 == len(grid) or grid[i + 1][j] == 0
+
     return perimeter
